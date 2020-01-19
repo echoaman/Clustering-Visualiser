9adf40c9398354ac904725342ccde32f996b1815
Aman Pahurkar <amanpahurkar13@gmail.com>
2020-01-19 19:09:07 +0530

    added event listeners

--- public/scripts/board.js
+++ public/scripts/board.js
@@ -13,47 +13,75 @@ const board_data = document.getElementById('board-data');
 let data = []
 let centroid = []
 
+const changeEvent = id => {
+    canvas.removeEventListener('click', generateSingleData, false);
+    canvas.removeEventListener('click', generateCentroid, false);
+    canvas.removeEventListener('click', deleteData, false);
+
+    switch (id) {
+        case 'data':
+            canvas.addEventListener('click', generateSingleData, false);
+            break;
+
+        case 'centroid':
+            canvas.addEventListener('click', generateCentroid, false);
+            break;
+
+        case 'remove':
+            canvas.addEventListener('click', deleteData, false);
+            break;
+
+        case 'clear':
+            break;
+
+        case 'visualize':
+            break;
+    }
+}
+
 const changeAlgo = id => {
-    for(let i = 0; i < algos.length; i++){
-        if(algos[i].id === id)
+    for (let i = 0; i < algos.length; i++) {
+        if (algos[i].id === id)
             algos[i].style.opacity = 1;
         else algos[i].style.opacity = 0.5;
-
-        if(id === 'dbscan'){
-            parameters[0].style.display = 'flex';
-            parameters[2].style.display = 'flex';
-            controllers[3].innerHTML = 'Remove Data';
-            controllers[2].style.display = 'none';
-        }else{
-            parameters[0].style.display = 'none';
-            parameters[2].style.display = 'none';
-            controllers[3].innerHTML = 'Remove Data/Centroid';
-            controllers[2].style.display = 'block';
-        }
     }
+    
+    if(id === 'dbscan'){
+        parameters[0].style.display = 'flex';
+        parameters[2].style.display = 'flex';
+        controllers[3].innerHTML = 'Remove Data';
+        controllers[2].style.display = 'none';
+    }else{
+        parameters[0].style.display = 'none';
+        parameters[2].style.display = 'none';
+        controllers[3].innerHTML = 'Remove Data/Centroid';
+        controllers[2].style.display = 'block';
+    }
+
 }
 
 const changeController = id => {
-    for(let i = 0; i < controllers.length; i++){
-        if(controllers[i].id === id)
+    for (let i = 0; i < controllers.length; i++) {
+        if (controllers[i].id === id)
             controllers[i].style.opacity = 1;
         else controllers[i].style.opacity = 0.5;
     }
+
+    changeEvent(id);
 }
 
 const init = () => {
-    for(let i = 0; i < algos.length; i++){
+    for (let i = 0; i < algos.length; i++) {
         algos[i].addEventListener('click', () => {
             changeAlgo(algos[i].id);
         });
     }
 
-    for(let i = 0; i < controllers.length; i++){
+    for (let i = 0; i < controllers.length; i++) {
         controllers[i].addEventListener('click', () => {
             changeController(controllers[i].id)
         });
     }
-
 
     changeAlgo('kmeans');
     changeController('data');
@@ -62,6 +90,21 @@ const init = () => {
     random_input.value = 100;
 }
 
+const generateSingleData = event => {
+    alert('single');
+}
+
+const generateCentroid = event => {
+    alert('centroid');
+}
+
+const deleteData = event => {
+    alert('delete');
+}
+
+const generateRandomData = num => {
+    alert(num);
+}
 
 //Events
 
@@ -75,29 +118,29 @@ neighbour_slider.addEventListener('change', () => {
 
 random_btn.addEventListener('click', () => {
     data_num = parseInt(random_input.value);
-    if(data_num < 0){
+    if (data_num < 0) {
         alert('Number of data points cannot be negative!');
-        board_data.innerHTML = 0;
+        data_num = 0;
         random_input.value = 0;
-    }else if(data_num > 1000){
+    } else if (data_num > 1000) {
         alert('Number of data points cannot be more than 1000!');
-        board_data.innerHTML = 1000;
+        data_num = 1000;
         random_input.value = 1000;
-    }else if(isNaN(data_num)){
+    } else if (isNaN(data_num)) {
         alert('Please enter a number!');
-        board_data.innerHTML = 0;
+        data_num = 0;
         random_input.value = 0;
     }
-    else{
-        board_data.innerHTML = data_num;
-    }
-}); 
+    
+    board_data.innerHTML = data_num;
+    generateRandomData(data_num)
+});
 
 random_input.addEventListener('keyup', (event) => {
-    if(event.keyCode === 13){
+    if (event.keyCode === 13) {
         random_btn.click();
     }
 })
 
 
-init()
+init();
