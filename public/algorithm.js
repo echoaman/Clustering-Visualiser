bb809cf953cc51e7703d439620458f77bb4327b3
Aman Pahurkar <amanpahurkar13@gmail.com>
2020-01-20 10:26:19 +0530

    added algorithm file

---
+++ public/algorithm.js
@@ -0,0 +1,15 @@
+const kmeans = (canvas, dataArray, centroidArray) => {
+
+}
+
+const kmedoids = (canvas, dataArray, centroidArray) => {
+
+}
+
+const fcm = (canvas, dataArray, centroidArray) => {
+
+}
+
+const dbscan = (canvas, dataArray) => {
+    
+}
